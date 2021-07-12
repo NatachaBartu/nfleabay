8faf2be5eafc6fb3091c77ba3efee51933110022
natacha <nat_bartu@hotmail.com>
2021-07-12 23:07:58 +1000

    adds enum to condition in listing, and radio buttons in view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+        def select_alert_subclass(input)
+        case input
+        when 'notice'
+        return 'info'
+        when 'sucess'
+            return 'sucess'
+        when 'error'
+            return 'danger'
+        when 'alert'
+            return 'warning'
+        else 
+            return 'info'
+        end
+    end
 end
